460e82c2c7a6208697228ed4dee16d71343fa5a9
GiovanniMarchetto <73216336+GiovanniMarchetto@users.noreply.github.com>
2022-01-02 20:20:20 +0100

    Simple firebase login, add a basic field and shirt bottom

---
+++ app/src/main/java/it/units/fantabasket/LoginActivity.java
@@ -0,0 +1,122 @@
+package it.units.fantabasket;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import it.units.fantabasket.databinding.ActivityLoginBinding;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+
+    @Override
+    public void onStart() {
+        Log.i("TAG","start");
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser != null) {
+            Log.i("TAG","not null user: "+currentUser.getDisplayName());
+            passToMainActivity();
+        }
+    }
+
+    private void passToMainActivity() {
+        //TODO: rivedere perché accesso provvisorio
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null) {
+            String welcome = "Benvenuto " + user.getEmail();
+            Toast.makeText(getApplicationContext(), welcome, Toast.LENGTH_LONG).show();
+
+            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+            intent.putExtra(MainActivity.user, user.getEmail());
+            startActivity(intent);
+            finish();
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_login);
+
+        ActivityLoginBinding binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        Log.i("TAG","create login activity");
+
+
+        mAuth = FirebaseAuth.getInstance();//inizializzo
+
+        final EditText username = binding.username;
+        final EditText password = binding.password;
+
+        TextWatcher validator = new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                binding.loginButton.setEnabled(!editable.toString().equals(""));
+            }
+        };
+
+        username.addTextChangedListener(validator);
+        password.addTextChangedListener(validator);
+
+        binding.loginButton.setOnClickListener(view ->
+                login(username.getText().toString(),
+                        password.getText().toString()));
+
+        binding.registrationButton.setOnClickListener(view ->
+                registration(username.getText().toString(),
+                        password.getText().toString()));
+    }
+
+    private void login(String email, String password) {
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, task -> {
+                    if (task.isSuccessful()) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d("TAG", "signInWithEmail:success");
+                        FirebaseUser user = mAuth.getCurrentUser();
+//                        updateUI(user);
+                        passToMainActivity();
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w("TAG", "signInWithEmail:failure", task.getException());
+                        Toast.makeText(getApplicationContext(), "Authentication failed.",
+                                Toast.LENGTH_SHORT).show();
+//                        updateUI(null);
+                    }
+                });
+    }
+
+    private void registration(String email, String password) {
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, task -> {
+                    if (task.isSuccessful()) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d("TAG", "createUserWithEmail:success");
+                        FirebaseUser user = mAuth.getCurrentUser();
+                        passToMainActivity();
+//                        updateUI(user);
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w("TAG", "createUserWithEmail:failure", task.getException());
+                        Toast.makeText(getApplicationContext(), "Authentication failed.",
+                                Toast.LENGTH_SHORT).show();
+//                        updateUI(null);
+                    }
+                });
+    }
+}
--- app/src/main/java/it/units/fantabasket/MainActivity.java
+++ app/src/main/java/it/units/fantabasket/MainActivity.java
@@ -1,13 +1,42 @@
 package it.units.fantabasket;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import androidx.appcompat.app.AppCompatActivity;
+import com.google.firebase.auth.FirebaseAuth;
+import it.units.fantabasket.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static String user = "Pinco pallino";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.i("TAG", "create main activity");
+
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        Intent intent = getIntent();
+        if (intent != null) {
+            String currentUser = intent.getStringExtra(user);
+            binding.text.setText(currentUser);
+        }
+
+        binding.exitButton.setOnClickListener(view -> returnToLogin());
+
+        binding.playmaker.setOnClickListener(view ->
+                binding.playmaker.setImageResource(R.drawable.shirt2)
+        );
+    }
+
+    private void returnToLogin() {
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+        mAuth.signOut();
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
