0e551ed66d6c54af0489e1195030170008ac49ce
GiovanniMarchetto <73216336+GiovanniMarchetto@users.noreply.github.com>
2022-01-06 11:48:43 +0100

    Add PlayerLayout

---
+++ app/src/main/java/it/units/fantabasket/PlayerLayout.java
@@ -0,0 +1,68 @@
+package it.units.fantabasket;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+@SuppressLint("ViewConstructor")
+public class PlayerLayout extends LinearLayout {
+
+    private final LinearLayout playerLayout;
+
+    public PlayerLayout(Context context, Player player) {
+        super(context);
+
+        LinearLayout.LayoutParams linearParams = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        linearParams.gravity = LinearLayout.TEXT_ALIGNMENT_CENTER;
+
+        playerLayout = new LinearLayout(context);
+        playerLayout.setOrientation(LinearLayout.VERTICAL);
+
+        Button button = new Button(context);
+        button.setText(player.number);
+        button.setBackground(getShirt(context, player.team));
+
+        final float scale = context.getResources().getDisplayMetrics().density;
+        int dp = 58;
+        int pixels = (int) (dp * scale + 0.5f);
+        linearParams.width = pixels;
+        linearParams.height = pixels;
+        button.setLayoutParams(linearParams);
+
+        TextView name = new TextView(context);
+        name.setText(player.name);
+//                name.setLayoutParams(linearParams);
+
+        playerLayout.addView(button);
+        playerLayout.addView(name);
+    }
+
+    private Drawable getShirt(Context context, Team team) {
+        int shirt;
+        switch (team) {
+            case ATHLETISMO:
+                shirt=R.drawable.shirt_athletismo;
+                break;
+            case GORIZIANA:
+                shirt=R.drawable.shirt_goriziana;
+                break;
+            case OLIMPIA:
+                shirt=R.drawable.shirt_olimpia;
+                break;
+            case ROMANS:
+                shirt=R.drawable.shirt2;
+                break;
+            default:
+                shirt=R.drawable.shirt;
+        }
+        return context.getDrawable(shirt);
+    }
+
+    public LinearLayout getPlayerLayout() {
+        return playerLayout;
+    }
+}
