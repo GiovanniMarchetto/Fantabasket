aa8ff90f3db4442a6ab9afaa5e348c52b8e28347
GiovanniMarchetto <73216336+GiovanniMarchetto@users.noreply.github.com>
2022-01-12 11:21:41 +0100

    Add FieldPositions enum (different from role)

---
+++ app/src/main/java/it/units/fantabasket/enums/FieldPositions.java
@@ -0,0 +1,16 @@
+package it.units.fantabasket.enums;
+
+public enum FieldPositions {
+    PLAYMAKER,
+    GUARDIA_DX,
+    GUARDIA_SX,
+    ALA,
+    CENTRO,
+    PANCHINA_1,
+    PANCHINA_2,
+    PANCHINA_3,
+    PANCHINA_4,
+    PANCHINA_5,
+    PANCHINA_6,
+    PANCHINA_7
+}
