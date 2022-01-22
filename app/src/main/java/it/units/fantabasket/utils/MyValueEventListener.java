87e36523ba01dc310f150bf53a911f03f1b2db54
GiovanniMarchetto <73216336+GiovanniMarchetto@users.noreply.github.com>
2022-01-22 23:41:50 +0100

    New interface for refactor

---
+++ app/src/main/java/it/units/fantabasket/utils/MyValueEventListener.java
@@ -0,0 +1,18 @@
+package it.units.fantabasket.utils;
+
+import android.util.Log;
+import androidx.annotation.NonNull;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.ValueEventListener;
+import org.jetbrains.annotations.NotNull;
+
+public interface MyValueEventListener extends ValueEventListener {
+
+    void onDataChange(@NonNull @NotNull DataSnapshot snapshot);
+
+    @Override
+    default void onCancelled(@NonNull @NotNull DatabaseError error) {
+        Log.e("MIO-ERRORE", error.getMessage() + System.lineSeparator() + error.getDetails());
+    }
+}
